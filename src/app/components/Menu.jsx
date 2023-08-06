c90b7d2d4d76d9413cc3d49826717c05fe17fc4b
Elias Gonzalez <eliasivang@gmail.com>
2023-08-06 20:54:02 -0300

    test

--- src/app/components/Menu.jsx
+++ src/app/components/Menu.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 function Menu() {
   return (
-    <div className='absolute right-0 z-50 w-3/5 text-left shadow-xl bg-neutral-900 rounded-xl'>
+    <div className='absolute right-0 z-50 w-9/12 text-left shadow-xl bg-neutral-900 rounded-xl'>
         <div className='px-3 py-3'>
             <ul className='grid gap-3 my-4 text-xl text-white'>
                 <li className='p-2 hover:bg-green-400 w-fit rounded-xl'>
-                       <a href="#aboutMe">SOBRE MI</a> 
+                      <a href="#aboutMe">SOBRE MI</a> 
                 </li>
                 <li className='p-2 hover:bg-green-400 w-fit rounded-xl'>
-                       <a href="#proyects">PORTFOLIO</a> 
+                      <a href="#proyects">PORTFOLIO</a> 
                 </li>
                 <li className='p-2 hover:bg-green-400 w-fit rounded-xl'>
-                       <a href="#contact">CONTACTO</a> 
+                      <a href="#contact">CONTACTO</a> 
                 </li>
             </ul>
         </div>
